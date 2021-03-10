8999f95d481c7879c6842f6e32a6463e5f1f3465
Maciej Korszuń <maciej_korszun@epam.com>
2021-03-10 10:53:43 +0100

    initial todos

--- issue01/issue1.js
+++ issue01/issue1.js
@@ -1,5 +1,18 @@
 const PORT = 3000;
 const express = require("express");
 const app = express();
+const Todo = require('./../schemas/Todo');
 
-console.log('working')
+console.log('app working')
+
+let currentTodos = [
+	new Todo(1, "Buy groceries"),
+	new Todo(2, "Dispose of garbage", true),
+	new Todo(Math.floor(Math.random() * 10), "Wash the floors", false)
+];
+
+app.get("/", function (req, res) {
+	res.send("Maciej Korszuń node.js training with Adilson Junior as mentor");
+});
+
+app.listen(PORT);
